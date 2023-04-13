255bb200f9f7f4af34958ab71091833570dd3b3b
shaunfroseth <shaunfroseth@gmail.com>
2023-04-12 21:47:39 -0700

    Testing Dictionary API. Seems to be working, need to parse json and display relevant info

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
+const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+const searchButton = document.getElementById("search-btn");
+
+async function loadData() {
+  const word = document.getElementById("input-field").value;
+  const response = await fetch(BASE_URL + word);
+  const data = await response.json();
+  console.log(data);
+}
+
+searchButton.addEventListener("click", loadData);
+
 let changeFontStyle = function (font) {
   document.getElementById("output-text").style.fontFamily = font.value;
 };
