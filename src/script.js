c36875a47466bd9b08edcfcebf2f9f343f1651e3
shaunfroseth <shaunfroseth@gmail.com>
2023-04-19 15:53:03 -0700

    Bulk data retrieved and displayed to frontend (definitions, part of word)

--- src/script.js
+++ src/script.js
@@ -1,15 +1,64 @@
 const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 const searchButton = document.getElementById("search-btn");
+const toggleSlider = document.getElementById("light-dark-toggle");
 
 async function loadData() {
   const word = document.getElementById("input-field").value;
   const response = await fetch(BASE_URL + word);
   const data = await response.json();
-  console.log(data);
+
+  return data;
 }
 
-searchButton.addEventListener("click", loadData);
+async function displayData() {
+  let result = await loadData();
+
+  let resultDiv = document.getElementById("results");
+  resultDiv.innerHTML = ""; //Clear innerHTML between searches
+
+  //Set h1 for retrieved word
+  let wordHeader = document.createElement("h1");
+  wordHeader.innerHTML = result[0].word;
+  resultDiv.appendChild(wordHeader);
+
+  //Create and append list of meanings
+  let meanings = result[0].meanings;
+  let meaningList = document.createElement("ul");
+  meaningList.setAttribute("class", "part-of-speech");
+  resultDiv.appendChild(meaningList);
+
+  //Append "partOfSpeech" and definitions for each partOfSpeech
+  for (let i = 0; i < meanings.length; i++) {
+    meaningList.innerHTML += "<li>" + meanings[i].partOfSpeech + "</li>";
+    let definitionList = document.createElement("ul");
+    definitionList.setAttribute("id", "definition-list");
+    meaningList.appendChild(definitionList);
+    for (let j = 0; j < meanings[i].definitions.length; j++) {
+      definitionList.innerHTML +=
+        "<li>" + meanings[i].definitions[j].definition + "</li>";
+    }
+  }
+
+  console.log(result);
+}
+
+function toggleTheme() {
+  document.body.classList.toggle("dark");
+  console.log("click");
+}
+
+toggleSlider.addEventListener("click", toggleTheme);
+
+// Checks for click or enter-press
+searchButton.addEventListener("click", displayData);
+document.getElementById("input-field").addEventListener("keyup", (event) => {
+  event.preventDefault();
+  if (event.key == "Enter") {
+    document.getElementById("search-btn").click();
+  }
+});
+//
 
 let changeFontStyle = function (font) {
   document.getElementById("output-text").style.fontFamily = font.value;
